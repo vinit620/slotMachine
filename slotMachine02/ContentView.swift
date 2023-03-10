9d7bf31c3a63b5ce40b63c1761345b985f8566e3
Vinit Jha <jhavinit01@gmail.com>
2023-03-10 14:22:57 +0530

    Initial Commit

---
+++ slotMachine02/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  slotMachine02
+//
+//  Created by Vinit Jha on 10/03/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ slotMachine02/slotMachine02App.swift
@@ -0,0 +1,17 @@
+//
+//  slotMachine02App.swift
+//  slotMachine02
+//
+//  Created by Vinit Jha on 10/03/23.
+//
+
+import SwiftUI
+
+@main
+struct slotMachine02App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
